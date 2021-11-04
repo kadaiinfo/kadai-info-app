45aa86764bea9b77e2564d61624627918ca86237
tiger0427 <capacitor0427@gmail.com>
2021-11-05 02:46:09 +0900

    add: firebase setting for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -9,6 +8,5 @@ Future<void> main() async {
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp, //縦固定
   ]);
-  await Firebase.initializeApp();
   runApp(const ProviderScope(child: App()));
 }

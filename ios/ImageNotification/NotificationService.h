06034bc78c2e5bc1184c654fc4f63de7e34ac6f6
tiger0427 <capacitor0427@gmail.com>
2021-11-05 20:33:10 +0900

    add: cloud messaging setting

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by 藤澤大河 on 2021/11/05.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end

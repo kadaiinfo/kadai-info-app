61b1e7208366e65ead816316e4151692c32bbad3
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-11-07 04:12:14 +0900

    add balance mock

---
+++ lib/presentation/balance/widget/balance_transaction_list/balance_transaction_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// 取引一覧
+class BalanceTransactionList extends StatelessWidget {
+  const BalanceTransactionList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
+  }
+}

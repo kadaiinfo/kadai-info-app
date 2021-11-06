7a47c2954398d2b1040ebfc03ff01c921bf40b11
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-11-07 04:14:55 +0900

    add circle

---
+++ lib/presentation/circle/circle_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// サークル一覧画面
+class CirclePage extends StatelessWidget {
+  const CirclePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}

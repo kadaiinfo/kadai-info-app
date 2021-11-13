54ea6847d8d7ab03983bcc58338fabf0b94f834b
tiger0427 <capacitor0427@gmail.com>
2021-11-13 18:32:52 +0900

    Update i_micro_cms_datasource.dart

--- lib/infrastructure/datasource/micro_cms/i_micro_cms_datasource.dart
+++ lib/infrastructure/datasource/micro_cms/i_micro_cms_datasource.dart
@@ -1,4 +1,3 @@
-import 'package:kadai_info_flutter/domain/entity/binanbijo2021/candidate.dart';
 import 'package:kadai_info_flutter/infrastructure/datasource/micro_cms/model/mc_binanbijo_list_response.dart';
 
 abstract class IMicroCmsDatasource {

8c417910ff67c7eec4c3403f495e024ce79f221b
tiger0427 <capacitor0427@gmail.com>
2022-04-22 23:59:04 +0900

    Fix firebase analytics error
    コメントアウトを戻した

--- lib/model/service/firebase_analytics.dart
+++ lib/model/service/firebase_analytics.dart
@@ -1,20 +1,20 @@
-// // Package imports:
-// import 'package:firebase_analytics/firebase_analytics.dart';
-// import 'package:hooks_riverpod/hooks_riverpod.dart';
+// Package imports:
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// final firebaseAnalyticsService = Provider((_) => FirebaseAnalyticsService());
+final firebaseAnalyticsService = Provider((_) => FirebaseAnalyticsService());
 
-// enum AnalyticsEvent { button, banner, scan }
+enum AnalyticsEvent { button, banner, scan }
 
-// class FirebaseAnalyticsService {
-//   static final FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.instance;
-//   static final FirebaseAnalyticsObserver observer =
-//       FirebaseAnalyticsObserver(analytics: firebaseAnalytics);
+class FirebaseAnalyticsService {
+  static final FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.instance;
+  static final FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: firebaseAnalytics);
 
-//   Future<void> sendEvent(
-//       {required AnalyticsEvent event,
-//       required Map<String, dynamic> parameterMap}) async {
-//     final eventName = event.toString().split('.')[1];
-//     firebaseAnalytics.logEvent(name: eventName, parameters: parameterMap);
-//   }
-// }
+  Future<void> sendEvent(
+      {required AnalyticsEvent event,
+      required Map<String, dynamic> parameterMap}) async {
+    final eventName = event.toString().split('.')[1];
+    firebaseAnalytics.logEvent(name: eventName, parameters: parameterMap);
+  }
+}
